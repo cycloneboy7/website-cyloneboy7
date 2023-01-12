88a48d8bd64d45313c4caa50dbe98ef1fe3ab991
cycloneboy7 <521akhilarjun@gmail.com>
2023-01-12 21:47:17 +0530

    alot changed

--- main.js
+++
@@ -1,6 +0,0 @@
-/*function hide() {
-    		document.getElementById('image').style.display = "none";
-}
-function show() {
-        document.getElementById('image').style.display = "block";
-    } */
